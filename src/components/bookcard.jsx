15cd51dad65b51f5c455097b67cb8de327ff1a11
Shashank Venkatesh <shashankvenkatesh7906@gmail.com>
2025-03-10 09:39:47 +0530

    final

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,14 @@
-// write the book component code here
+import React from "react";
+
+const BookCard = ({ book }) => {
+  return (
+    <div>
+      <img src={book.image} alt={book.name} />
+      <h2>{book.name}</h2>
+      <p >Genre: {book.genre}</p>
+      <p>Author: {book.author}</p>
+    </div>
+  );
+};
+
+export default BookCard;
